bee3167d2cb98a5b32127ba3d1573c58327f46f7
Karen Carnaval <karencarnaval4@gmail.com>
2021-07-11 22:50:12 -0400

    auto log-in fix

--- src/login.js
+++ src/login.js
@@ -11,15 +11,19 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(function (data) {
             const user = firebase.auth().currentUser;
             console.log('User logged in')
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
-        }
-    });
+
             firebase.auth().onAuthStateChanged(function (user) {
                 if (user) {
                     window.location = "index.html";
                 }
             });
+
+            })
+
+            .catch(function (error) {
+                console.error(error);
+            });
+        }
+    });
+            
         });
